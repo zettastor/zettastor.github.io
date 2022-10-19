e1779a959d2cdef947e5cd2e280a9c96033be3b6
zettastor <lanewu@gmail.com>
2022-10-19 09:08:11 -0500

    fixed doc link

--- src/config.mjs
+++ src/config.mjs
@@ -5,7 +5,7 @@ export const SITE = {
   basePathname: "/",
 
   title: "ZettaStor DBS",
-  description: "🚀 ZettaStor DBS is a free and ready to use framework.",
+  description: "ZettaStor DBS is a free and ready to use framework.",
 
   googleAnalyticsId: false, // or "G-XXXXXXXXXX",
   googleSiteVerificationId: "orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M",
@@ -30,32 +30,32 @@ export const BLOG = {
 };
 
 export const SIDEBAR = {
-	en: {
+  en: {
     'User': [
-			{ text: 'OS Installation', link: SITE.basePathname + '/os' },
-      { text: 'Configuration and Deployment', link: SITE.basePathname + '/install' },
-			{ text: 'Configuration Guide', link: SITE.basePathname + '/configuration' },
-      { text: 'User Manual', link: SITE.basePathname + '/manual' },
-		],
-		'Developer': [
-      { text: 'Compilation', link: SITE.basePathname + '/compile' },
-      { text: 'Developer Guide', link: SITE.basePathname + '/devguide' },
-      { text: 'Coding Standard', link: SITE.basePathname + '/codingstyle' },
-      { text: 'License', link: SITE.basePathname + '/agpl-3.0' },
+      { text: 'OS Installation', link: SITE.basePathname + 'operatingsystem' },
+      { text: 'Configuration and Deployment', link: SITE.basePathname + 'install' },
+      { text: 'Configuration Guide', link: SITE.basePathname + 'configuration' },
+      { text: 'User Manual', link: SITE.basePathname + 'manual' },
     ],
-	},
-	zh: {
+    'Developer': [
+      { text: 'Compilation', link: SITE.basePathname + 'compile' },
+      { text: 'Developer Guide', link: SITE.basePathname + 'devguide' },
+      { text: 'Coding Standard', link: SITE.basePathname + 'codingstyle' },
+      { text: 'License', link: SITE.basePathname + 'agpl-3.0' },
+    ],
+  },
+  zh: {
     '用户文档': [
-			{ text: '安装操作系统', link: SITE.basePathname + '/os' },
-      { text: '配置与部署', link: SITE.basePathname + '/install' },
-			{ text: '详细配置说明', link: SITE.basePathname + '/configuration' },
-      { text: '用户手册', link: SITE.basePathname + '/manual' },
-		],
-		'开发文档': [
-      { text: '编译说明', link: SITE.basePathname + '/compile' },
-      { text: '开发指南', link: SITE.basePathname + '/devguide' },
-      { text: '编码规范', link: SITE.basePathname + '/codingstyle' },
-      { text: '许可证', link: SITE.basePathname + '/agpl-3.0' },
+      { text: '安装操作系统', link: SITE.basePathname + 'operatingsystem' },
+      { text: '配置与部署', link: SITE.basePathname + 'install' },
+      { text: '详细配置说明', link: SITE.basePathname + 'configuration' },
+      { text: '用户手册', link: SITE.basePathname + 'manual' },
     ],
-	},
+    '开发文档': [
+      { text: '编译说明', link: SITE.basePathname + 'compile' },
+      { text: '开发指南', link: SITE.basePathname + 'devguide' },
+      { text: '编码规范', link: SITE.basePathname + 'codingstyle' },
+      { text: '许可证', link: SITE.basePathname + 'agpl-3.0' },
+    ],
+  },
 };
